845e4c3d26040b9299dde30982f7bffd84dfdba7
Mikko Rautiainen <m_rautiainen@hotmail.com>
2021-12-23 10:43:18 +0200

    crosslink removal works with sequencing errors as well

--- scripts/remove_crosslink_paths.py
+++ scripts/remove_crosslink_paths.py
@@ -20,27 +20,31 @@ def endnode(p):
 	path = p.replace('>', '\t>').replace('<', '\t<').strip().split('\t')
 	return revnode(path[-1])
 
+def canontip(n1, n2):
+	if n2 + n1 < n1 + n2: return (n2, n1)
+	return (n1, n2)
+
 def check_side(start):
-	global forbidden_paths
-	if start not in paths_per_unique: return
-	if len(paths_per_unique[start]) != 2: return
+	global forbidden_connections
+	if start not in connections_per_unique: return
+	if len(connections_per_unique[start]) != 2: return
 	other_side = set()
-	used_paths = set()
-	for path in paths_per_unique[start]:
-		other_side.add(path[1])
-		used_paths.add(path[0])
+	used_connections = set()
+	for node in connections_per_unique[start]:
+		other_side.add(node)
+		used_connections.add(canontip(start, node))
 	if len(other_side) != 2: return
 	has_two = False
 	this_side = set()
 	has_many = False
 	for side in other_side:
-		for path in paths_per_unique[side]:
-			if len(paths_per_unique[side]) == 2: has_two = True
-			if len(paths_per_unique[side]) > 2: has_many = True
-			for side2 in paths_per_unique[side]:
-				this_side.add(side2[1])
-				used_paths.add(side2[0])
-				if len(paths_per_unique[side2[1]]) > 2: has_many = True
+		for path in connections_per_unique[side]:
+			if len(connections_per_unique[side]) == 2: has_two = True
+			if len(connections_per_unique[side]) > 2: has_many = True
+			for side2 in connections_per_unique[side]:
+				this_side.add(side2)
+				used_connections.add(canontip(side, side2))
+				if len(connections_per_unique[side2]) > 2: has_many = True
 	if not has_two: return
 	if has_many: return
 	if len(this_side) != 2: return
@@ -48,19 +52,19 @@ def check_side(start):
 	for node in this_side: total_used_nodes.add(node[1:])
 	for node in other_side: total_used_nodes.add(node[1:])
 	if len(total_used_nodes) != 4: return
-	if len(used_paths) < 3: return
-	if len(used_paths) > 4: return
-	abundant_paths = list(used_paths)
-	abundant_paths.sort(key=lambda x: -path_counts[x])
-	assert path_counts[abundant_paths[1]] >= path_counts[abundant_paths[2]]
-	if path_counts[abundant_paths[1]] == path_counts[abundant_paths[2]]: return
+	if len(used_connections) < 3: return
+	if len(used_connections) > 4: return
+	abundant_connections = list(used_connections)
+	abundant_connections.sort(key=lambda x: -connection_counts[x])
+	assert connection_counts[abundant_connections[1]] >= connection_counts[abundant_connections[2]]
+	if connection_counts[abundant_connections[1]] == connection_counts[abundant_connections[2]]: return
 	used_here = set()
-	used_here.add(startnode(abundant_paths[0]))
-	used_here.add(startnode(abundant_paths[1]))
-	used_here.add(endnode(abundant_paths[0]))
-	used_here.add(endnode(abundant_paths[1]))
+	used_here.add(abundant_connections[0][0])
+	used_here.add(abundant_connections[0][1])
+	used_here.add(abundant_connections[1][0])
+	used_here.add(abundant_connections[1][1])
 	if len(used_here) < 4: return
-	for i in range(2, len(abundant_paths)): forbidden_paths.add(abundant_paths[i])
+	for i in range(2, len(abundant_connections)): forbidden_connections.add(abundant_connections[i])
 
 
 unique_nodes = set()
@@ -68,31 +72,45 @@ with open(unique_nodes_file) as f:
 	for l in f:
 		unique_nodes.add(l.strip())
 
-paths_per_unique = {}
+paths_per_connection = {}
+connections_per_unique = {}
 all_paths = set()
 with open(picked_paths_file) as f:
 	for l in f:
 		path = l.replace('>', '\t>').replace('<', '\t<').strip().split('\t')
 		if len(path) == 0: continue
 		all_paths.add(l.strip())
-		if path[0] not in paths_per_unique: paths_per_unique[path[0]] = set()
-		paths_per_unique[path[0]].add((l.strip(), revnode(path[-1])))
-		if revnode(path[-1]) not in paths_per_unique: paths_per_unique[revnode(path[-1])] = set()
-		paths_per_unique[revnode(path[-1])].add((l.strip(), path[0]))
+		if path[0] not in connections_per_unique: connections_per_unique[path[0]] = set()
+		connections_per_unique[path[0]].add(revnode(path[-1]))
+		if revnode(path[-1]) not in connections_per_unique: connections_per_unique[revnode(path[-1])] = set()
+		connections_per_unique[revnode(path[-1])].add(path[0])
+		key = canontip(path[0], path[-1])
+		if key not in paths_per_connection: paths_per_connection[key] = set()
+		paths_per_connection[key].add(l.strip())
 
-path_counts = {}
+connection_counts = {}
 with open(counted_paths_file) as f:
 	for l in f:
-		if l.strip() not in path_counts: path_counts[l.strip()] = 0
-		path_counts[l.strip()] += 1
+		if len(l.strip()) == 0: continue
+		path = l.strip().replace('>', '\t>').replace('<', '\t<').strip().split('\t')
+		if len(path) == 0: continue
+		key = canontip(path[0], revnode(path[-1]))
+		if key not in connection_counts: connection_counts[key] = 0
+		connection_counts[key] += 1
 
-forbidden_paths = set()
+forbidden_connections = set()
 for node in unique_nodes:
 	check_side('>' + node)
 	check_side('<' + node)
 
-for path in all_paths:
-	if path not in forbidden_paths:
-		print(path)
+for pair in forbidden_connections:
+	sys.stderr.write("removed connection: " + str(pair) + "\n")
+
+for pathstr in all_paths:
+	path = pathstr.replace('>', '\t>').replace('<', '\t<').strip().split('\t')
+	if len(path) == 0: continue
+	key = canontip(path[0], revnode(path[-1]))
+	if key not in forbidden_connections:
+		print(pathstr)
 	else:
-		sys.stderr.write("forbade " + path + "\n")
+		sys.stderr.write("forbade " + pathstr + "\n")
