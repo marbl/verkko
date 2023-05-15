1dbc6b92716d97044100950654b512310d683cfc
steven-solar <steven.j.solar@gmail.com>
2023-05-15 14:04:50 -0400

    new python scripts

---
+++ src/scripts/merge_layouts.py
@@ -0,0 +1,43 @@
+import re
+import sys
+
+hifi_layout = open(sys.argv[1])
+ont_layout = open(sys.argv[2])
+
+lines = dict()
+for hifi_line in hifi_layout:
+    if hifi_line[:3] == 'tig':
+        hifi_tig_line = hifi_line.strip()
+    elif hifi_line[:3] == 'len':
+        hifi_len_line = hifi_line.strip()
+    elif hifi_line[:3] == 'rds':
+        hifi_num_reads = int(hifi_line.strip().split('\t')[1])
+    elif re.match('^[a-zA-Z0-9-_\/]+\t\d+\t\d+$', hifi_line):
+        hifi_line_split = hifi_line.split('\t')
+        hifi_pos = min(int(hifi_line_split[1]), int(hifi_line_split[2]))
+        lines[hifi_line.strip() + '\t0'] = hifi_pos
+    elif hifi_line[:3] == 'end':
+        ont_num_reads = -1
+        keep_lines = False
+        printed_header_and_hifi = False
+        for ont_line in ont_layout:
+            if ont_line.strip() == hifi_tig_line:
+                keep_lines = True
+            if keep_lines and ont_line[:3] == 'rds':
+                ont_num_reads = int(ont_line.strip().split('\t')[1])
+            if keep_lines and ont_num_reads >= 0 and not printed_header_and_hifi:
+                print(hifi_tig_line)
+                print(hifi_len_line)
+                print('rds\t' + str(hifi_num_reads + ont_num_reads))
+                printed_header_and_hifi = True
+                hifi_lines = list()
+            if keep_lines and re.match('^[a-zA-Z0-9-_\/]+\t\d+\t\d+$', ont_line):
+                ont_line_split = ont_line.split('\t')
+                ont_pos = min(int(ont_line_split[1]), int(ont_line_split[2]))
+                lines[ont_line.strip() + '\t1'] = ont_pos
+            if ont_line[:3] == 'end':
+                sorted_lines = dict(sorted(lines.items(), key=lambda x:x[1]))
+                for k,v in sorted_lines.items():
+                    print(k)
+                print('end')
+                break
---
+++ src/scripts/replace_path_nodes.py
@@ -0,0 +1,45 @@
+import sys
+import re
+
+def reverse_dir(dir):
+    if dir == '>':
+        return '<'
+    elif dir == '<':
+        return '>'
+
+def reverse_path(path, regex_expr):
+    out_path = ''
+    node_names = re.findall(regex_expr, path)
+    node_dirs = re.findall('[<>]', path)
+    for i, node_name in (enumerate(node_names)):
+        out_path = reverse_dir(node_dirs[i]) + node_name + out_path
+    return out_path
+
+def process_map_file(map_file):
+    utig_map = dict()
+    for line in map_file:
+        line_split = line.strip().split('\t')
+        utig_node = line_split[0]
+        nodeID_path = line_split[1].split(':')[0]
+        utig_map[utig_node] = nodeID_path
+    return utig_map
+
+in_gaf = open(sys.argv[1])
+map_file = open(sys.argv[2])
+
+utig_to_nodeIDPath = process_map_file(map_file)
+for line in in_gaf:
+    line_split = line.strip().split('\t')
+    path = line_split[5]
+    node_names = re.findall('utig1-\d+', path)
+    node_dirs = re.findall('[<>]', path)
+    new_path = ''
+    for i in range(len(node_names)):
+        node_dir = node_dirs[i]
+        node_name = node_names[i]
+        if node_dir == '>':
+            new_path += utig_to_nodeIDPath[node_name]
+        elif node_dir == '<':
+            new_path += reverse_path(utig_to_nodeIDPath[node_name], '[<>]([a-zA-Z0-9-_]*)')
+    line_split[5] = new_path
+    print('\t'.join(line_split))
