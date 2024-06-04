85336490c5672025bd237875ca65ecf682790e18
Dmitry Antipov <dmitrij.antipov@gmail.com>
2024-06-04 06:14:02 -0400

    crash fx

--- src/scripts/get_paths_fasta.py
+++ src/scripts/get_paths_fasta.py
@@ -4,14 +4,14 @@ import sys
 import os
 import graph_functions 
 from scaffolding import path_storage
+import networkx as nx
 
-
-def main():
-    rukki_path = sys.argv[1]
-    fasta_file = sys.argv[2]
-    graph = sys.argv[3]
-    out_fasta = sys.argv[4]
-    G = graph_functions.load_direct_graph(graph)
-    path_storage = path_storage.PathStorage(G)
-    path_storage.readFromFile(rukki_path)
-    path_storage.writeFasta(fasta_file, out_fasta)
+rukki_path = sys.argv[1]
+fasta_file = sys.argv[2]
+graph_file = sys.argv[3]
+out_fasta = sys.argv[4]
+G = nx.DiGraph()
+graph_functions.load_direct_graph(graph_file, G )
+path_storage = path_storage.PathStorage(G)
+path_storage.readFromFile(rukki_path)
+path_storage.writePathAsFasta(fasta_file, out_fasta)
